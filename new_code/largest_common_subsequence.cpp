5b296014dfe7fc4abef415ea2829e94fcdd11ed3
zhang_qinzheng <2259227276@qq.com>
2024-05-16 16:53:00 +0800

    [Yanyi][20240516_1652] largest_com_sequence algorithm:dp is nb

---
+++ new_code/largest_common_subsequence.cpp
@@ -0,0 +1,49 @@
+/* 查找两个字符串中的最长公共子序列 
+======================================================
+数据范围：0≤∣str1∣,∣str2∣≤20000≤∣str1∣,∣str2∣≤2000
+要求：空间复杂度 O(n2)O(n2) ，时间复杂度 O(n2)O(n2) 
+======================================================
+输入：
+"1A2C3D4B56","B1D23A456A"
+返回值：
+"123456"
+*/
+
+
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * longest common subsequence
+     * @param s1 string字符串 the string
+     * @param s2 string字符串 the string
+     * @return string字符串
+     */
+    string LCS(string s1, string s2) {
+        int len1 = s1.length() + 1;
+        int len2 = s2.length() + 1;
+        string res = "";
+        vector<vector<int> > dp(len1, vector<int>(len2, 0));
+        for (int i = 1; i < len1; ++i)
+            for (int j = 1; j < len2; ++j)
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+ 
+        int i = len1 - 1, j = len2 - 1;
+        while (dp[i][j]) {
+            if (dp[i-1][j] == dp[i][j-1] && dp[i][j] > dp[i-1][j-1]) {
+                res += s1[i - 1];
+                --i;
+                --j;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1])  --i;
+            else --j;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
